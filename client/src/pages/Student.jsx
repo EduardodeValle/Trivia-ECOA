c63bc17025cb409afb68a880d23a67cdf6b1f033
Eduardo de Valle <jsvalle50@gmail.com>
2023-04-24 21:02:04 -0600

    Se cambio el juego por otra version mejorada para el navegador, aun se siguen haciendo modificaciones para que se vea aceptable

--- client/src/pages/Student.jsx
+++ client/src/pages/Student.jsx
@@ -3,6 +3,8 @@ import { useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Student.css';
 
+// las propiedades del videojuego estan en el tag <iframe>, el videojuego se encuentra en el frame contenedor
+
 function Student() {
   const location = useLocation();
   const welcomeText = location.state.welcomeText;
@@ -14,7 +16,7 @@ function Student() {
       <div className="row justify-content-center">
         <div className="col-md-8">
           <div className="game-container bg-light p-3">
-            <iframe src="../Videogame/index.html" width="800" height="600"></iframe>
+            <iframe src="../Videogame/index.html" width="300px" height="100px"></iframe> 
           </div>
         </div>
       </div>
